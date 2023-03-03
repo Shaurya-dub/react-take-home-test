c1861794c2dee529e45c6c26d9db863b72664ea5
Shaurya-dub <shauryadubey@hotmail.ca>
2023-03-03 11:59:42 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App container">
+      <h1 className='text-center'>Brew Ninja Test App</h1>
+    </div>
+  );
+}
+
+export default App;
